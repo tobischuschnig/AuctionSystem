b1b0b247c800f0fb24096476475c119deaa4757b
tobischuschnig <tschuschnig@student.tgm.ac.at>
2013-12-10 18:18:36 +0100

    Server
    Server laut uml

---
+++ src/server/Server.java
@@ -0,0 +1,20 @@
+package server;
+
+import java.util.ArrayList;
+
+public class Server implements Runnable{
+
+	private int tcpPort;
+	private ArrayList<User> user;
+	private ArrayList<Auction> auction;
+	
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
---
+++ src/server/UserHandler.java
@@ -0,0 +1,14 @@
+package server;
+
+public class UserHandler implements Runnable{
+
+	private String host;
+	private int tcpPort;
+	private Server server;
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+}
